58c8072fb14976d00e33805c0085845132ea5856
Rodrigo Salsa <rodrigosalsag@hotmail.com>
2023-08-15 16:12:02 -0300

    Códigos referenciando um cachorro

---
+++ cachorro.java
@@ -0,0 +1,18 @@
+package MeuProjetoJava;
+
+public class cachorro {
+
+	int tamanho;
+	String raça;
+	String nome;
+	
+	
+	
+	
+	void nome() {System.out.println("Nome: Jorge.");}
+	void raça() {System.out.println("Raça: Labrador");}
+	void tamanho() {System.out.println("Tamanho: 1 metro");}
+	void latir() {System.out.println("Latido: Au, auuu, auuuu, auuu! Me dá uma comida ai hahahaha");
+	}
+
+}
---
+++ cachorroteste.java
@@ -0,0 +1,16 @@
+package MeuProjetoJava;
+
+public class cachorroteste {
+
+	public static void main(String[] args) {
+		
+		cachorro viralata = new cachorro();
+		viralata.nome();
+		viralata.raça();
+		viralata.tamanho();
+		viralata.raça = "viralata";
+		viralata.tamanho = 100;
+		viralata.latir();
+	}
+
+}
